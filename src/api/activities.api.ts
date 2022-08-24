e6fb6c41fc49f951d271b2d2bc6d807414adfcd0
Csaba Kiss <ckiss@equinor.com>
2022-08-24 10:09:44 +0200

    Separation of concerns

--- src/api/activities.api.ts
+++ src/api/activities.api.ts
@@ -1,35 +1,12 @@
-import { useEffect, useState } from "react";
 import { baseURL } from "../config";
-import { ApiResponse, ActivityItem } from "./activities.api.types";
+import { ActivityItem } from "./activities.api.types";
 
-
-export function useApi<T>(url: string): ApiResponse<T> {
-    const [data, setData] = useState<any>();
-    const [error, setError] = useState<any>();
-
-    useEffect(() => {
-        async function fetchAPI() {
-            try {
-                const response = await fetch(baseURL + url);
-                const json = await response.json();
-                setData(json);
-            } catch (error) {
-                setError(error);
-            }
-        }
-        fetchAPI();
-    }, []);
-
-    return { data, error };
-}
-
-
-export async function getAllActivities(): Promise<{data: Promise<ActivityItem[]>; error: any}> {
+export async function getAllActivities(): Promise<{data: ActivityItem[]; error: any}> {
     try {
       const response = await fetch(baseURL + "activities_get")
       const json = await response.json();
       return {data: json, error: undefined};
     } catch (error) {
-      return {data: Promise.resolve([]), error};
+      return {data: [], error};
     }
 }
--- src/components/Activity.tsx
+++ src/components/Activity.tsx
@@ -2,33 +2,18 @@ import ActivityCard from './ActivityCard'
 import { useEffect, useState } from 'react';
 import { baseURL } from '../config';
 import { ActivityItem } from '../api/activities.api.types';
+import { useAllActivities } from '../hooks/useAllActivities';
 //import { GroupState, useSolvannApiWithHeaders } from '../API/SolvannAPI';
 
 
 const Activity: React.FC = () => {
-  // const [data, setData] = useState<any>();
-  // const [error, setError] = useState<any>();
-
-  // const [activities, setActivities] = useState<ActivityItem[]>([]);
-
-  // useEffect(() => {
-  //   getActivities();
-  // }, []);
-
-  // const getActivities = async () => {
-  //   try {
-  //     const response = await fetch(baseURL + "activities_get");
-  //     //.then((response) => response.json());
-  //     const json = await response.json();
-  //     setActivities(json)
-  //     console.log("activities", json)
-  //   } catch (error) {
-  //     setError(error);
-  //     console.log("error")
-  //   }
-  // }
+  const allActivities = useAllActivities();
 
   //activities.forEach((item) => item.)
+
+  useEffect(() => {
+    console.warn('allActivities', allActivities)
+  }, [allActivities])
 
   return (
     <>
