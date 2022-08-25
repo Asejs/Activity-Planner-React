b2c4577f63479e3c2ffd4c22e66d48f9472b6fc6
Asejs <Asejs@github.com>
2022-08-25 13:47:21 +0200

    Added the loading indicator, using setTimeout

--- src/components/Activity.tsx
+++ src/components/Activity.tsx
@@ -1,15 +1,24 @@
 import ActivityCard from './ActivityCard'
 import { useAllActivities } from '../hooks/useAllActivities';
+import { CircularProgress } from '@mui/material';
 
 
 const Activity: React.FC = () => {
-  const allActivities = useAllActivities().data;
+  const allActivities = useAllActivities().apiResponse.data;
+  const loading = useAllActivities().loading;
+  console.log(loading)
+
+  const loadingStyle = {
+    display: 'flex',
+    mr: 'auto',
+    ml: 'auto' 
+  }
 
   return (
     <>
-      {allActivities.map((item) =>
-        <ActivityCard activityItem={item} key={item.activity_id} />
-      )}
+      { loading || allActivities === undefined ?
+        <CircularProgress color="success" sx={ loadingStyle }/>
+        : allActivities.map((item) => <ActivityCard activityItem={item} key={item.activity_id} /> ) }
     </>
   );
 }
--- src/hooks/useAllActivities.ts
+++ src/hooks/useAllActivities.ts
@@ -1,18 +1,20 @@
 import { useEffect, useState } from "react";
-import { baseURL } from "../config";
-import { ApiResponse, ActivityItem } from "../api/activities.api.types";
+import { ActivityItem } from "../api/activities.api.types";
 import { getAllActivities } from "../api/activities.api";
 
 export function useAllActivities() {
-    const [apiResponse, setApiResponse] = useState({ data: [] as ActivityItem[], error: undefined });
+    const [apiResponse, setApiResponse] = useState({ data: [] as ActivityItem[], error: undefined});
+    const [loading, setLoading] = useState<boolean>(false);
 
     useEffect(() => {
+        setLoading(true);
         async function fetchAPI() {
             const response = await getAllActivities();
             setApiResponse(response);
+            setLoading(false)
         }
-        fetchAPI();
+        setTimeout( () => { fetchAPI(); }, 2000)
     }, []);
 
-    return apiResponse;
+    return { apiResponse, loading };
 }
